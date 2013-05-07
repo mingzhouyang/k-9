42843fb27b3b70ccd04aa971a355fe76adecd3fb
Haitao.Ji <vendola_jee@126.com>
2013-05-07 21:27:27 +0800

    Add mock loigc for crypt method, will be removed after intergration

--- src/com/fsck/k9/mail/cryptography/AESCommonCryptoImpl.java
+++ src/com/fsck/k9/mail/cryptography/AESCommonCryptoImpl.java
@@ -10,11 +10,17 @@ public class AESCommonCryptoImpl implements ICrypto {
 
 	@Override
 	public String encrypto(String content, String password) throws CryptorException {
+		if("".equals(password)){
+			return "Mock: " + content;
+		}
 		return AESEncryptor.encrypt(content, password); 
 	}
 
 	@Override
 	public String decrypto(String content, String password) throws CryptorException {
+		if("".equals(password)){
+			return "Mock: " + content;
+		}
 	    return AESEncryptor.decrypt(content, password);
 	}
 
